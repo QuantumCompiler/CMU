fd8a0177bca2ca2664ede8b3c2a51e0affedefb7
QuantumCompiler <tjlarrechea@gmail.com>
2024-03-30 21:34:14 -0700

    Gen Updates 3/30/24

--- Scripts/ExtClean.sh
+++ Scripts/ExtClean.sh
@@ -1,1 +1,2 @@
-find .. \( -name "*.atfi" -o -name "*.aux" -o -name "*.bcf" -o -name "*.log" -o -name "*.out" -o -name "*.ptc" -o -name "*.xml" \) -type f -exec rm '{}' \;
+find .. \( -name "*.atfi" -o -name "*.aux" -o -name "*.bcf" -o -name "*.log" -o -name "*.out" -o -name "*.ptc" -o -name "*.xml" -o -name ".DS_Store" \) -type f -exec rm '{}' \;
+
